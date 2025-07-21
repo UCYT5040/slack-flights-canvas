89530e8ac55b1ff7b031533da8c084e47b3fa03e
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-21 14:02:32 -0500

    Class and interface for themes

---
+++ src/theme.ts
@@ -0,0 +1,53 @@
+import {IconOptions} from './marker';
+import {tileLayer, TileLayer} from 'leaflet';
+
+interface ThemeOptions {
+    name: string;  // Name of the theme
+    tile_layer: {
+        url: string;  // URL of the tile layer
+        attribution: string;  // Attribution text for the tile layer
+        ext: string;  // File extension for the tile layer images
+    };
+    icons: {
+        airplane: IconOptions;
+        airport: IconsOptions;
+    }
+}
+
+
+export class Theme {
+    private options: ThemeOptions;
+    private pois: { string: IconOptions } = {};  // Points of Interest (POIs) icons for this theme
+
+    constructor(options: ThemeOptions) {
+        this.options = options;
+    }
+
+    registerPOI(name: string, icon: IconOptions): void {
+        if (this.pois[name]) {
+            throw new Error(`POI with name ${name} already exists.`);
+        }
+        this.pois[name] = icon;
+    }
+
+    getPOI(name: string): IconOptions | undefined {
+        return this.pois[name];
+    }
+
+    getIcon(name: string): IconOptions | undefined {
+        return this.icons[name];
+    }
+
+    getTileLayer(): TileLayer {
+        return tileLayer(this.options.tile_layer.url, {
+            minZoom: 1,
+            maxZoom: 16,
+            attribution: this.options.tile_layer.attribution,
+            ext: this.options.tile_layer.ext
+        });
+    }
+
+    getName(): string {
+        return this.options.name;
+    }
+}
